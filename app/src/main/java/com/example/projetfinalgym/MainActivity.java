0029468922c6b245b89008cdc19481dd70539831
zafer <lssjshisui@gmail.com>
2023-04-27 12:14:42 -0400

    SignUp

--- app/src/main/java/com/example/projetfinalgym/MainActivity.java
+++ app/src/main/java/com/example/projetfinalgym/MainActivity.java
@@ -7,11 +7,11 @@ import android.os.Bundle;
 import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
     }
 
     public void ViewSignUp(View view) {
--- app/src/main/java/com/example/projetfinalgym/SignUpActivity.java
+++ app/src/main/java/com/example/projetfinalgym/SignUpActivity.java
@@ -1,12 +1,65 @@
 package com.example.projetfinalgym;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.auth.UserProfileChangeRequest;
+
 public class SignUpActivity extends AppCompatActivity {
+    FirebaseAuth mAuth;
+    String name, email, password;
+
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sign_up_);
+        name = findViewById(R.id.name).toString();
+        email = findViewById(R.id.email).toString();
+        password = findViewById(R.id.password).toString();
+
+        // Initialize Firebase Auth
+        mAuth = FirebaseAuth.getInstance();
+
     }
+    private void CreateUser() {
+        if (!name.isEmpty()) {
+            Toast.makeText(SignUpActivity.this, "La case Nom ne doit pas etre vide",
+                    Toast.LENGTH_SHORT).show();
+        }
+        else {
+            // create user
+            mAuth.createUserWithEmailAndPassword(email, password)
+                    .addOnCompleteListener(this, task -> {
+                        if (task.isSuccessful()) {
+                            // Sign in success
+                            // set le nom du user
+                            FirebaseUser user = mAuth.getCurrentUser();
+                            UserProfileChangeRequest profileUpdateRequest = new UserProfileChangeRequest.Builder()
+                                    .setDisplayName(name).build();
+                            user.updateProfile(profileUpdateRequest);
+                        } else {
+                            // If sign in fails, display a message to the user.
+                            Toast.makeText(SignUpActivity.this, "Authentication failed.",
+                                    Toast.LENGTH_SHORT).show();
+                        }
+                    });
+
+        }
+    }
+    public void SignUp(View view) {
+        this.CreateUser();
+    }
+
+
 }
