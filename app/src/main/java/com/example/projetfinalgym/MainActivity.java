34fbc43c5e082ca49dc86f761234474c078cd55e
zafer <lssjshisui@gmail.com>
2023-04-28 22:36:50 -0400

    main

--- app/src/main/java/com/example/projetfinalgym/MainActivity.java
+++ app/src/main/java/com/example/projetfinalgym/MainActivity.java
@@ -1,6 +1,12 @@
 package com.example.projetfinalgym;
 
+import android.os.Bundle;
+
 import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
-}
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+    }
