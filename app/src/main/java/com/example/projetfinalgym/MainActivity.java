0dabb3ac796e8ecc8d7f3aa1132f07f67f6c3728
zafer <lssjshisui@gmail.com>
2023-05-04 10:07:17 -0400

    Bonjour message

--- app/src/main/java/com/example/projetfinalgym/MainActivity.java
+++ app/src/main/java/com/example/projetfinalgym/MainActivity.java
@@ -1,12 +1,20 @@
 package com.example.projetfinalgym;
 
 import android.os.Bundle;
+import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
+
+import com.google.firebase.auth.FirebaseAuth;
 
 public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        FirebaseAuth auth = FirebaseAuth.getInstance();
+        String userName = auth.getCurrentUser().getDisplayName();
+        TextView bonjour = findViewById(R.id.textView7);
+        bonjour.setText("Hi " + userName);
     }
     }
