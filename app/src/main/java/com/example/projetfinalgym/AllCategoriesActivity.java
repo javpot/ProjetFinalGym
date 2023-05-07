a052f362e1c1b2abc3fa18a6e70c9cc237b68943
zafer <lssjshisui@gmail.com>
2023-05-07 08:34:43 -0400

    donnees ajoute a la base

--- app/src/main/java/com/example/projetfinalgym/AllCategoriesActivity.java
+++ app/src/main/java/com/example/projetfinalgym/AllCategoriesActivity.java
@@ -8,18 +8,12 @@ import com.google.firebase.firestore.FirebaseFirestore;
 
 public class AllCategoriesActivity extends AppCompatActivity {
 
-    Workout workoutTriceps1 = new Workout();
-    Workout workoutChest1 = new Workout();
-    Workout workoutDos1 = new Workout();
-    Workout workoutEtirements1 = new Workout();
-    Workout workoutCardio1 = new Workout();
-    Workout workoutJambes1 = new Workout();
-    Workout workoutEpaules1 = new Workout();
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_all_categories);
+
+        FirebaseFirestore db = FirebaseFirestore.getInstance();
 
 
 
