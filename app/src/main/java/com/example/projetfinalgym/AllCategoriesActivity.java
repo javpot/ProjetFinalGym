64319482651ec196e7bf53b738d1d6875472dd5f
zafer <lssjshisui@gmail.com>
2023-05-08 12:34:42 -0400

    requete a la base

--- app/src/main/java/com/example/projetfinalgym/AllCategoriesActivity.java
+++ app/src/main/java/com/example/projetfinalgym/AllCategoriesActivity.java
@@ -1,22 +1,25 @@
 package com.example.projetfinalgym;
 
+import android.content.Intent;
 import android.os.Bundle;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.firestore.FirebaseFirestore;
 
 public class AllCategoriesActivity extends AppCompatActivity {
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_all_categories);
-
-        FirebaseFirestore db = FirebaseFirestore.getInstance();
-
-
-
     }
 
+    private void TransferCategorieParameter(Categories categories) {
+        Intent intent = new Intent(this, AllWorkoutsActivity.class);
+        intent.putExtra("categorie", categories);
+        startActivity(intent);
+
+    }
 }
--- app/src/main/java/com/example/projetfinalgym/AllWorkoutsActivity.java
+++ app/src/main/java/com/example/projetfinalgym/AllWorkoutsActivity.java
@@ -5,10 +5,25 @@ import android.os.Bundle;
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.firestore.FirebaseFirestore;
+
 public class AllWorkoutsActivity extends AppCompatActivity {
+    FirebaseAuth mAuth;
+
+    Categories categorie = (Categories) getIntent().getSerializableExtra("categorie");
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_all_workouts);
+
+        FirebaseFirestore db = FirebaseFirestore.getInstance();
+        FirebaseUser user = mAuth.getCurrentUser();
+
+        db.collection("Users").document(user.getUid()).collection("Workout")
+                .whereEqualTo("categorie", categorie)
+                .get();
+
     }
 }
